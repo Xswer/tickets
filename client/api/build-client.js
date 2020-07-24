62c923f3ec30ce6c979a3b19a36690e83ce0a72d
Kirill Pahl <kirill.pahl@digiwrld.com>
2020-07-24 18:02:56 +0200

    fix ingress and client for DO

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 export default ({ req }) => {
   if (typeof window === 'undefined') {
     return axios.create({
-      baseURL: 'http://nginx-ingress-controller.kube-system.svc.cluster.local',
+      // baseURL: 'http://nginx-ingress-controller.kube-system.svc.cluster.local',
+      baseURL: 'http://www.tickets-best.xyz',
       headers: req.headers,
     });
   } else {
